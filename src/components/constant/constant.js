8a2c173fe8f48aacc373a7e73c3749968a0bbf6c
mr7aali <mr7aali@gmail.com>
2023-10-02 23:39:18 +0600

    more image video page done

---
+++ src/components/constant/constant.js
@@ -0,0 +1,14 @@
+export const ProjectBgImageAndVideoLink = [
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2022/02/thumbnail-beautiful-3d-simulation.jpg", projectLink: "//vimeo.com/668065461#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2018/05/elements-product-video.jpg", projectLink: "//vimeo.com/272762225#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2022/11/Decentralized-Finance-LiveAction-Isometric-Film.jpg", projectLink: "//vimeo.com/769869333#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2020/04/covid19-awareness-3d-thumbnail.jpg", projectLink: "//vimeo.com/410919765#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2022/11/Susten-Japanese-3D-Animation.jpg", projectLink: "//vimeo.com/768573075#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2021/10/Accedian-High-Tech-Isometric-Animation.jpg", projectLink: "//vimeo.com/562126788#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2021/10/Velco-2D-Animated-Explainer-Video.jpg", projectLink: "//vimeo.com/584269174#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2017/12/Pipeline-3d-animation-thumb.jpg", projectLink: "//vimeo.com/139916370#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2017/12/Legacy.jpg", projectLink: "//vimeo.com/140168093#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2023/09/Upwise-Fuel-Explainer-Video.jpg", projectLink: "//vimeo.com/860803588#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2020/08/Asae-100-years-animation-image.jpg", projectLink: "//vimeo.com/446434385#s" },
+    { bgImage: "https://www.kasradesign.com/wp-content/uploads/2018/07/ceo-talking-head-liveaction-animation.jpg", projectLink: "//vimeo.com/277675580#s" },
+  ];
--- src/components/sheared/MoreVideoAdded.js
+++ src/components/sheared/MoreVideoAdded.js
@@ -1,4 +1,17 @@
+import Link from "next/link";
+import { ProjectBgImageAndVideoLink } from "../constant/constant";
 
+
+
+const Project = ({ bgImage, projectLink }) => {
+  return (
+    <div class="grid-item">
+      <div class="grid-item__image" style={{ backgroundImage: `url(${bgImage})` }}>
+        <Link href={projectLink} class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></Link>
+      </div>
+    </div>
+  )
+}
 
 const MoreVideoAdded = () => {
   return (
@@ -8,11 +21,11 @@ const MoreVideoAdded = () => {
           <div class="section__title">
             <h4 class="h2">More videos from our portfolio</h4>
 
-            <p>There is plenty to see in our portfolio. Take a look <br/>
+            <p>There is plenty to see in our portfolio. Take a look <br />
               at some of our favourite videos!</p>
           </div>
 
-          <div class="section__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2017/11/title-with-image1.jpg)"}}>
+          <div class="section__image" style={{ backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2017/11/title-with-image1.jpg)" }}>
 
           </div>
         </div>
@@ -20,70 +33,14 @@ const MoreVideoAdded = () => {
 
       <div class="section__body">
         <div class="grid grid--small">
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2022/02/thumbnail-beautiful-3d-simulation.jpg)"}}>
-              <a href="//vimeo.com/668065461#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2018/05/elements-product-video.jpg)"}}>
-              <a href="//vimeo.com/272762225#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2022/11/Decentralized-Finance-LiveAction-Isometric-Film.jpg)"}}>
-              <a href="//vimeo.com/769869333#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2020/04/covid19-awareness-3d-thumbnail.jpg)"}}>
-              <a href="//vimeo.com/410919765#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2022/11/Susten-Japanese-3D-Animation.jpg)"}}>
-              <a href="//vimeo.com/768573075#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2021/10/Accedian-High-Tech-Isometric-Animation.jpg)"}}>
-              <a href="//vimeo.com/562126788#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2021/10/Velco-2D-Animated-Explainer-Video.jpg)"}}>
-              <a href="//vimeo.com/584269174#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2017/12/Pipeline-3d-animation-thumb.jpg)"}}>
-              <a href="//vimeo.com/139916370#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2017/12/Legacy.jpg)"}}>
-              <a href="//vimeo.com/140168093#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2023/09/Upwise-Fuel-Explainer-Video.jpg)"}}>
-              <a href="//vimeo.com/860803588#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2020/08/Asae-100-years-animation-image.jpg)"}}>
-              <a href="//vimeo.com/446434385#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
-          <div class="grid-item">
-            <div class="grid-item__image" style={{backgroundImage: "url(https://www.kasradesign.com/wp-content/uploads/2018/07/ceo-talking-head-liveaction-animation.jpg)"}}>
-              <a href="//vimeo.com/277675580#s" class="video-trigger" data-wpel-link="external" rel="nofollow external noopener noreferrer"></a>
-            </div>
-          </div>
+          {
+            ProjectBgImageAndVideoLink.map((p, index) => <Project key={index} bgImage={p.bgImage} projectLink={p.projectLink} />)
+          }
+
         </div>
       </div>
 
-      
+
 
       <div class="section__actions">
         <a href="https://www.kasradesign.com/portfolio/" class="btn btn--red" data-wpel-link="internal">Check  our entire portfolio</a>
