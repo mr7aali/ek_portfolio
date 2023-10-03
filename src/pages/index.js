9b9a798908ea6c62477de0af5a53963ccf57fc2d
mr7aali <mr7aali@gmail.com>
2023-10-03 22:37:26 +0600

    used dynamic import for good performance

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,41 @@
-import ClintSection from "@/components/HomePage/ClintSection";
-import ClintSectionWithSponsore from "@/components/HomePage/ClintSectionWithSponsore";
-import Description from "@/components/HomePage/Description";
-import FreequentlyAsked from "@/components/HomePage/FreequentlyAsked";
-import HomeIntro from "@/components/HomePage/HomeIntro";
-import HomePageBanner from "@/components/HomePage/HomePageBanner";
-import LestsTalk from "@/components/HomePage/LestsTalk";
-import LetsTalk2 from "@/components/HomePage/LestsTalk2";
-import MixedProject from "@/components/HomePage/MixedProject";
-import MoreVideoAdded from "@/components/HomePage/MoreVideoAdded";
-import VideoPlayModal from "@/components/HomePage/VideoPlayModal";
-import Footer from "@/components/sheared/Footer";
-import HeadTag from "@/components/sheared/HeadTag";
-import NavBar from "@/components/sheared/NavBar";
+import dynamic from "next/dynamic";
+
+const  ClintSectionWithSponsore= dynamic(()=>import("@/components/HomePage/ClintSectionWithSponsore"));
+const  ClintSection= dynamic(()=>import("@/components/HomePage/ClintSection"));
+const Description = dynamic(() => import("@/components/HomePage/Description"));
+const FreequentlyAsked = dynamic(() => import("@/components/HomePage/FreequentlyAsked"));
+const HomeIntro = dynamic(() => import("@/components/HomePage/HomeIntro"));
+const HomePageBanner = dynamic(() => import("@/components/HomePage/HomePageBanner"));
+const LestsTalk = dynamic(() => import("@/components/HomePage/LestsTalk"));
+const LetsTalk2 = dynamic(() => import("@/components/HomePage/LestsTalk2"));
+const MixedProject = dynamic(() => import("@/components/HomePage/MixedProject"));
+const MoreVideoAdded = dynamic(() => import("@/components/HomePage/MoreVideoAdded"));
+const VideoPlayModal = dynamic(() => import("@/components/HomePage/VideoPlayModal"));
+const Footer = dynamic(() => import("@/components/sheared/Footer"));
+const HeadTag = dynamic(() => import("@/components/sheared/HeadTag"));
+const NavBar = dynamic(() => import("@/components/sheared/NavBar"));
+
+
+
+// import Description from "@/components/HomePage/Description";
+// import FreequentlyAsked from "@/components/HomePage/FreequentlyAsked";
+// import HomeIntro from "@/components/HomePage/HomeIntro";
+// import HomePageBanner from "@/components/HomePage/HomePageBanner";
+// import LestsTalk from "@/components/HomePage/LestsTalk";
+// import LetsTalk2 from "@/components/HomePage/LestsTalk2";
+// import MixedProject from "@/components/HomePage/MixedProject";
+// import MoreVideoAdded from "@/components/HomePage/MoreVideoAdded";
+// import VideoPlayModal from "@/components/HomePage/VideoPlayModal";
+// import Footer from "@/components/sheared/Footer";
+// import HeadTag from "@/components/sheared/HeadTag";
+// import NavBar from "@/components/sheared/NavBar";
+
+
 import { motion, useScroll } from "framer-motion";
 import { useState } from "react";
+
+
+
 
 
 
@@ -42,7 +64,7 @@ export default function Home() {
                    {/* /this component has some error */}
         <div style={{border:"1px solid red"}} className="h-[31px]">
         </div>
-        <LetsTalk2/>  
+        {/* <LetsTalk2/>   */}
         <FreequentlyAsked />
         <Footer />
         <VideoPlayModal open={open} setOpen={setOpen} />
