2ce1f24db840d35c1bd571ed5acd75bcdc2fe707
mr7aali <mr7aali@gmail.com>
2023-10-04 16:27:01 +0600

    progress bar fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
-
-const  ClintSectionWithSponsore= dynamic(()=>import("@/components/HomePage/ClintSectionWithSponsore"));
-const  ClintSection= dynamic(()=>import("@/components/HomePage/ClintSection"));
+const ClintSectionWithSponsore = dynamic(() => import("@/components/HomePage/ClintSectionWithSponsore"));
+const ClintSection = dynamic(() => import("@/components/HomePage/ClintSection"));
 const Description = dynamic(() => import("@/components/HomePage/Description"));
 const FreequentlyAsked = dynamic(() => import("@/components/HomePage/FreequentlyAsked"));
 const HomeIntro = dynamic(() => import("@/components/HomePage/HomeIntro"));
@@ -14,28 +13,8 @@ const VideoPlayModal = dynamic(() => import("@/components/HomePage/VideoPlayModa
 const Footer = dynamic(() => import("@/components/sheared/Footer"));
 const HeadTag = dynamic(() => import("@/components/sheared/HeadTag"));
 const NavBar = dynamic(() => import("@/components/sheared/NavBar"));
-
-
-
-// import Description from "@/components/HomePage/Description";
-// import FreequentlyAsked from "@/components/HomePage/FreequentlyAsked";
-// import HomeIntro from "@/components/HomePage/HomeIntro";
-// import HomePageBanner from "@/components/HomePage/HomePageBanner";
-// import LestsTalk from "@/components/HomePage/LestsTalk";
-// import LetsTalk2 from "@/components/HomePage/LestsTalk2";
-// import MixedProject from "@/components/HomePage/MixedProject";
-// import MoreVideoAdded from "@/components/HomePage/MoreVideoAdded";
-// import VideoPlayModal from "@/components/HomePage/VideoPlayModal";
-// import Footer from "@/components/sheared/Footer";
-// import HeadTag from "@/components/sheared/HeadTag";
-// import NavBar from "@/components/sheared/NavBar";
-
-
 import { motion, useScroll } from "framer-motion";
 import { useState } from "react";
-
-
-
 
 
 
@@ -55,21 +34,19 @@ export default function Home() {
       <NavBar />
       <HomePageBanner />
       <HomeIntro />
-        <MixedProject />
-        <Description />
-        <MoreVideoAdded />
-        <ClintSection />
-        <ClintSectionWithSponsore />
-        {/* <LestsTalk />   */}
-                   {/* /this component has some error */}
-        <div style={{border:"1px solid red"}} className="h-[31px]">
-        </div>
-        {/* <LetsTalk2/>   */}
-        <FreequentlyAsked />
-        <Footer />
-        <VideoPlayModal open={open} setOpen={setOpen} />
-
-
+      <MixedProject />
+      <Description />
+      <MoreVideoAdded />
+      <ClintSection />
+      <ClintSectionWithSponsore />
+      {/* <LestsTalk />   */}
+      {/* /this component has some error */}
+      <div style={{ border: "1px solid red" }} className="h-[31px]">
+      </div>
+      {/* <LetsTalk2/>   */}
+      <FreequentlyAsked />
+      <Footer />
+      <VideoPlayModal open={open} setOpen={setOpen} />
     </div>
   )
 }
