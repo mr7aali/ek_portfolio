62cb9eab4538b15cb6f04ef618e0f0f8fd1865a3
Aali <mr7aali@gmail.com>
2025-05-12 11:16:52 +0600

    a

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+      </Head>
       {/* page-template page-template-templates page-template-inner-page page-template-templatesinner-page-php page page-id-1510 page-loaded has-menu-opened */}
-      <body className='page-id-1510 page-loaded'>
+      <body className="page-id-1510 page-loaded">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,68 @@
 import dynamic from "next/dynamic";
-const ClintSectionWithSponsore = dynamic(() => import("@/components/HomePage/ClintSectionWithSponsore"));
-const ClintSection = dynamic(() => import("@/components/HomePage/ClintSection"));
+const ClintSectionWithSponsore = dynamic(() =>
+  import("@/components/HomePage/ClintSectionWithSponsore")
+);
+const ClintSection = dynamic(() =>
+  import("@/components/HomePage/ClintSection")
+);
 const Description = dynamic(() => import("@/components/HomePage/Description"));
-const FreequentlyAsked = dynamic(() => import("@/components/HomePage/FreequentlyAsked"));
+const FreequentlyAsked = dynamic(() =>
+  import("@/components/HomePage/FreequentlyAsked")
+);
 const HomeIntro = dynamic(() => import("@/components/HomePage/HomeIntro"));
-const HomePageBanner = dynamic(() => import("@/components/HomePage/HomePageBanner"));
+const HomePageBanner = dynamic(() =>
+  import("@/components/HomePage/HomePageBanner")
+);
 const LestsTalk = dynamic(() => import("@/components/HomePage/LestsTalk"));
 const LetsTalk2 = dynamic(() => import("@/components/HomePage/LestsTalk2"));
-const MixedProject = dynamic(() => import("@/components/HomePage/MixedProject"));
-const MoreVideoAdded = dynamic(() => import("@/components/HomePage/MoreVideoAdded"));
-const VideoPlayModal = dynamic(() => import("@/components/HomePage/VideoPlayModal"));
+const MixedProject = dynamic(() =>
+  import("@/components/HomePage/MixedProject")
+);
+const MoreVideoAdded = dynamic(() =>
+  import("@/components/HomePage/MoreVideoAdded")
+);
+const VideoPlayModal = dynamic(() =>
+  import("@/components/HomePage/VideoPlayModal")
+);
 const Footer = dynamic(() => import("@/components/sheared/Footer"));
 const HeadTag = dynamic(() => import("@/components/sheared/HeadTag"));
 const NavBar = dynamic(() => import("@/components/sheared/NavBar"));
 import { motion, useScroll } from "framer-motion";
 import { useState } from "react";
 
-
-
 export default function Home() {
   const [open, setOpen] = useState(false);
   const { scrollYProgress } = useScroll();
 
   return (
-
     <div>
-      <HeadTag title={'Home'} descriptionContent={"Tech products home page"} />
+      <HeadTag title={"Home"} descriptionContent={"Tech products home page"} />
       <motion.div
         className="progress-bar"
-        style={{ border: '5px solid #ff0055', scaleX: scrollYProgress, zIndex: 2000 }}
+        style={{
+          border: "5px solid #ff0055",
+          scaleX: scrollYProgress,
+          zIndex: 2000,
+        }}
       />
 
       <NavBar />
       <HomePageBanner />
       <HomeIntro />
 
-
-      
       <MixedProject />
       <Description />
-      <MoreVideoAdded  open={open} setOpen={setOpen}/>
+      <MoreVideoAdded open={open} setOpen={setOpen} />
       <ClintSection />
       <ClintSectionWithSponsore />
 
-
-
-      {/* <LestsTalk />   */}
+      {/* <LestsTalk /> */}
       {/* /this component has some error */}
-      <div style={{ border: "1px solid red" }} className="h-[31px]">
-      </div>
-      {/* <LetsTalk2/>   */}
+      {/* <div style={{ border: "1px solid red" }} className="h-[31px]"></div> */}
+      <LetsTalk2 />
       <FreequentlyAsked />
       <Footer />
       <VideoPlayModal open={open} setOpen={setOpen} />
     </div>
-  )
+  );
 }
